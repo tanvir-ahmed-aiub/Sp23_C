16ed22f61015596b43d4e80edf77fabb9798bd29
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-02-13 15:24:18 +0600

    LINQ and EF Intro

---
+++ EFIntro/EFIntro/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using EFIntro.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Register() { 
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Student model)  {
+            //validation
+            //db insert
+            var db = new UMSSp23_CEntities();
+            db.Students.Add(model);
+            //
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+        public ActionResult List() {
+            var db = new UMSSp23_CEntities();
+            var students = db.Students.ToList();
+            return View(students);
+        }
+        public ActionResult Edit(int id) {
+            var db = new UMSSp23_CEntities();
+            var st = (from s in db.Students
+             where s.Id == id
+             select s).SingleOrDefault();
+            return View(st);    
+        }
+        [HttpPost]
+        public ActionResult Edit(Student upstudent) {
+            var db = new UMSSp23_CEntities();
+            var exst = (from s in db.Students
+                        where s.Id == upstudent.Id
+                        select s).SingleOrDefault();
+            exst.Name = upstudent.Name;
+            exst.Profession = upstudent.Profession;
+            exst.Gender = upstudent.Gender;
+            exst.Dob = upstudent.Dob;
+
+            //db.Entry(exst).CurrentValues.SetValues(upstudent);
+            db.SaveChanges();
+
+
+            //db.Students.Remove(exst);
+            return RedirectToAction("List");
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        static void PrintList(List<Student> students) { 
+            foreach(var s in students)
+            {
+
+                s.Show();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] marks = new int[] {45,65,12,56,78,89,88,86,85 };
+            /*int[] aplus = new int[marks.Length];
+            int count=0;
+            foreach (var item in marks)
+            {
+                if (item >= 80) aplus[count++] = item;
+            }*/
+            var apuls = (from item in marks
+             where item >= 80
+             select item).ToArray();
+            List<Student> students  = new List<Student>();
+            Random rand = new Random();
+            for (int i = 1; i <= 100; i++) {
+                students.Add(new Student() {
+                    Id = i,
+                    Name = "Student "+ i,
+                    Marks = rand.Next(10,100)
+                });
+            }
+            PrintList(students);
+            var scstudents = (from s in students
+                             where s.Marks>=85
+                             select s).ToList();
+            Console.WriteLine("--------Scholarships "+scstudents.Count+"---------");
+            PrintList(scstudents);
+            var sc110= (from s in students
+                        where s.Marks >=85 && s.Id<=10
+                        select s).ToList();
+            Console.WriteLine("--------Scholarships++ " + sc110.Count + "---------");
+            PrintList(sc110);
+
+        }
+    }
+}
